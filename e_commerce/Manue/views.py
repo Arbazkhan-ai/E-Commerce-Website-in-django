d57bd44f2d5d7fae52f02596a6bf0178b0865811
Arbaz khan <arbazkhanofficial140@gmail.com>
2026-08-29 20:59:39 +0500

    Create template and Database

--- e_commerce/Manue/views.py
+++ e_commerce/Manue/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import Student
+
+
 def home(request):
-    return HttpResponse("Welcome to my website  ")
+    students = Student.objects.all()
+
+    return render(request, 'index.html', {
+        'students': students
+    })
